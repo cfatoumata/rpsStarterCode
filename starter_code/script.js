b82397b797f925222b08112b3f53613b6dd97339
cfatoumata <fatoumatacondhva2019@gmail.com>
2016-12-14 22:59:41 +0000

    rockpaperscissors

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,30 +1,30 @@
 // Task 1: Create a function that randomly chooses among 'rock', 'paper', or 'scissors'.
-var generateComputerChoice = function () {
+var generateComputerChoice = function() {
     // Task 1, Step 1: Create an array with three elements ("rock", "paper" and "scissors").
+    var rps = ["Rock", "Paper", "Scissors"]
+        // Task 1, Step 2: Use the JavaScript Math function to generate a random whole
+      var y = (Math.floor(Math.random() * 3));
         
-    // Task 1, Step 2: Use the JavaScript Math function to generate a random whole
-    // number between 0 and 2. Be sure to save it to a variable.
-        
-    // Task 1, Step 3: Use this randomly generated number to pull a value from the
-    // array (eg myArray[randomNum])
-        
-    // Task 1, Step 4: return this new value
-        
+        // Task 1, Step 3: Use this randomly generated number to pull a value from the
+        // array (eg myArray[randomNum])
+    var g = rps[y];
+        // Task 1, Step 4: return this new value
+       return (g);
 };
 
 // Task 2: Create a function that compares the userChoice and the computerChoice
 // to decide who won.
-var pickWinner = function (userChoice, computerChoice) {
+var pickWinner = function(userChoice, computerChoice) {
     console.log("user choice: " + userChoice);
     console.log("computer choice: " + computerChoice);
     // Task 2, Step 1: Create an "if/else if/else" statement that compares the
     // userChoice and computerChoice under any possible game outcome.
-       
+
     // Task 2, Step 2: Depending on who is the winner of the game console.log
     // either "user wins", "computer wins" or "draw"
 
     // Task 4: Show `computerChoice` in HTML after the words "Computer's choice:"
-    
+
     // Task 5: Show the winner in HTML after the word "Winner:"
 };
 
@@ -48,6 +48,6 @@ $(document).ready(function() {
     pickWinner(userChoice, computerChoice);
     //Final Task: comment out the pickWinner function on the line above and place it within your click functions created in Task 3.
     //this is done so the computer does not pick its throw until the user has also done so.
-    
+
 
 });
